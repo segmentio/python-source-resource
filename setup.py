a628c17139a04a907c524694d15f977c9949dc8c
Tejas Manohar <me@tejas.io>
2016-06-14 21:29:36 -0500

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.0.5',
+    version='0.0.6',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
@@ -12,6 +12,6 @@ setup(
     install_requires=[
         'pydash==3.4.3',
         'python-dateutil==2.5.3',
-        'segment_source==0.0.4'
+        'segment_source==0.0.5'
     ]
 )
