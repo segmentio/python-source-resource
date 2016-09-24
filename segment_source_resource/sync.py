ed176708b315f88734cc655900033f1870142cb5
Andrey Bulgakov <mail@andreiko.ru>
2016-09-23 17:41:16 -0700

    Changes to sync -> transform -> set pipeline (#2)

--- segment_source_resource/sync.py
+++ segment_source_resource/sync.py
@@ -3,6 +3,7 @@ import sys
 from gevent.pool import Group
 
 from segment_source_resource.exceptions import PublicError
+from segment_source_resource.resource import RawObj, Obj
 from segment_source import client as source
 
 
@@ -26,11 +27,17 @@ def _create_error_handler(collection):
 
 
 def _process_resource(resources, seed, resource):
-    for obj in resource.fetch(seed):
-        morphed = resource.transform(obj, seed)
-        resource.set(morphed)
+    for raw_obj in resource.fetch(seed):
+        if not isinstance(raw_obj, (Obj, RawObj)):
+            raw_obj = RawObj(data=raw_obj, collection=resource.collection, schema=resource.schema)
 
-        _enqueue_children(resources, obj, resource)
+        if isinstance(raw_obj, Obj):
+            obj = raw_obj
+        else:
+            obj = resource.transform(raw_obj, seed)
+
+        source.set(obj.collection, obj.id, obj.properties)
+        _enqueue_children(resources, raw_obj, resource)
 
 
 def _enqueue_children(resources, seed, parent):
