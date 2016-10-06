ffaad86c3bda78b5f176e4a60a2db230cef3463f
Tejas Manohar <tejas@segment.com>
2016-10-06 14:54:39 -0700

    raise RunError instead of calling sys.exit directly

--- segment_source_resource/exceptions.py
+++ segment_source_resource/exceptions.py
@@ -2,3 +2,14 @@ class PublicError(RuntimeError):
 
     def __init__(self, message):
         super(RuntimeError, self).__init__(message)
+
+
+class RunError(RuntimeError):
+
+    def __init__(self, message, errors):
+        self._message = message
+        super(RuntimeError, self).__init__(message)
+        self._errors = errors
+
+    def __str__(self):
+        print("{}: {}", self._message, self._errors)
