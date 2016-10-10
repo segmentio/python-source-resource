2a42235cbebbbb7520d1c2e76992feaa758e2349
Tejas Manohar <tejas@segment.com>
2016-10-10 11:55:59 -0700

    Release v0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.9.0',
+    version='0.9.1',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
