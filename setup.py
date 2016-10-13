79694a9200bd6b9eeac4dd749341c95e6acdfa55
Andrey Bulgakov <andrey@segment.com>
2016-10-13 14:52:37 -0700

    Release v0.9.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.9.1',
+    version='0.9.2',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
