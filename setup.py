43f8e0c53fc189f305281ae2147cda74da3a5d22
Dean Karn <dean@segment.com>
2017-08-03 14:36:18 -0700

    Release v0.10.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.9.11',
+    version='0.10.0',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
