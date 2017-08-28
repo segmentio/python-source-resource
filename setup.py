5fcc82b21c147b14f4a0e5727feae51b6baefc1b
Andrey Bulgakov <mail@andreiko.ru>
2017-08-28 16:06:25 -0700

    Release v0.11.0

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.10.0',
+    version='0.11.0',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
     url='https://github.com/segmentio/python-source-resource',
     install_requires=[
         'pydash==3.4.3',
-        'gevent==1.2.1',
+        'gevent==1.2.2',
         'python-dateutil==2.6.0',
-        'segment_source==0.10.1'
+        'segment_source==0.11.3',
     ],
     tests_require=['pytest'],
     setup_requires=['pytest-runner'],
