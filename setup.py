2c0748e27416fce612759326bd663fe0896a0e0f
Chris Demwell <chris.demwell@segment.com>
2017-11-23 16:57:30 -0800

    Release v0.12.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.12.0',
+    version='0.12.1',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
