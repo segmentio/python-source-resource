75bdece0c9407ddfca002d1d26c1f9a80633c971
Dean Karn <dean.karn@gmail.com>
2019-03-06 13:20:45 -0800

    fix segment_source version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'pydash',
         'gevent',
         'python-dateutil',
-        'segment_source==0.16.0',
+        'segment_source==0.17.0',
         'structlog',
     ],
     tests_require=['pytest'],
