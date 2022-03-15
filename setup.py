ea63cd6b8e7cabdb105368d157ebcf6db592576f
Priyanka Sankaranarayanan <priyanka.sankaranarayan@segment.com>
2022-03-15 22:32:34 +0530

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.16.1',
+    version='0.16.2',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
