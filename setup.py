3a6d1640d3c3db3d15cee923df1377d3fb92e549
Nithin Benny <nithin.benny@segment.com>
2023-08-07 14:53:17 +0530

    Update segment source resource version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.25.0',
+    version='0.26.0',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
