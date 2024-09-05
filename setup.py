aa24ae76943fd26de358faedfb54dfe2bebebbad
Pooja Srinivasan <53825520+PoojaSrinivasan18@users.noreply.github.com>
2024-09-05 12:25:46 +0530

    Update segment-source version (#29)
    * update version
    
    * update version
    
    ---------
    
    Co-authored-by: Pooja Srinivasan <pooja.srinivasan>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='segment_source_resource',
     packages=['segment_source_resource'],
-    version='0.26.0',
+    version='0.27.0',
     description='Abstraction to make sources easier to write',
     author='Segment',
     author_email='friends@segment.com',
@@ -12,7 +12,7 @@ setup(
         'pydash',
         'gevent',
         'python-dateutil',
-        'segment_source==0.25.0',
+        'segment_source==0.26.0',
         'structlog',
     ],
     tests_require=['pytest'],
