5ec6c07f184449718c069b0f378ca6c7c1f32cdb
Pooja Srinivasan <pooja.srinivasan>
2024-10-15 15:18:32 +0530

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 if [ -n "$(git describe --tags --abbrev=0)" ]; then
-  ver=$(python3 setup.py --version)
-  name=$(python3 setup.py --name)
+  ver=$(git describe --tags --abbrev=0 | sed 's/^v//')
+  name="segment_source_resource"
+  echo "${name}-${ver}.tar.gz"
   package_cloud push segment/py-wheels/python dist/${name}-${ver}.tar.gz
 else
   echo "skipping (no tag)"
